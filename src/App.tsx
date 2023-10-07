3f9ad9cec8ce80ea5a1a76d6eb0acff9691e80cc
Luiz Paulo Lindenmaier <lindennerd@gmail.com>
2023-10-07 00:34:34 -0300

    qrcode generator

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import QRCode from "qrcode";
+import { useRef } from "react";
+import "./App.css";
+
+function App() {
+  const linkRef = useRef<HTMLInputElement>(null);
+  const qrCodeCanvasRef = useRef<HTMLCanvasElement>(null);
+
+  function generateQRCode() {
+    var link = linkRef.current?.value;
+    if (!link) return;
+    QRCode.toCanvas(qrCodeCanvasRef.current, link, function (error) {
+      if (error) console.error(error);
+      console.log("success!");
+    });
+  }
+
+  function copyQrCode() {
+    if (qrCodeCanvasRef?.current == null) return;
+    const canvasAsImage = new ClipboardItem({
+      "image/png": qrCodeCanvasRef.current.toDataURL(),
+    });
+    navigator.clipboard.write([canvasAsImage]);
+  }
+
+  return (
+    <div className="page">
+      <input
+        type="text"
+        name="link"
+        placeholder="Link to QR Code!"
+        ref={linkRef}
+      />
+      <button onClick={() => generateQRCode()}>Gerar</button>
+
+      <canvas ref={qrCodeCanvasRef}></canvas>
+      <div>{/* <button onClick={(e) => copyQrCode()}>Copiar</button> */}</div>
+    </div>
+  );
+}
+
+export default App;
